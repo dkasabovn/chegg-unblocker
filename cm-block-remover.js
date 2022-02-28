4a470f75d1cab064e64064eeb2c110a3ed10cda7
dkasabovn <dkasabovn@gmail.com>
2022-02-27 20:21:41 -0600

    mac work

--- cm-block-remover.js
+++ cm-block-remover.js
@@ -4,7 +4,18 @@ function removeBlock() {
 		element.classList.toggle('cs-dm-block');
 	});
 	let blockingModal = document.getElementById("C-common-devicemanager-block");
-	blockingModal.remove();
+	if (blockingModal) {
+		blockingModal.remove();
+	}
+
+	let nonOverflowMac = document.querySelectorAll('.cs-dm-add');
+	nonOverflowMac.forEach(element => {
+		element.classList.toggle('cs-dm-add');
+	});
+	let blockingModalMac = document.getElementById("C-common-devicemanager-add");
+	if (blockingModalMac) {
+		blockingModalMac.remove();
+	}
 }
 
 window.addEventListener("load", removeBlock, false);
